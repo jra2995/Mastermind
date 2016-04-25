15bf9fd8c399d5dc88744247e4c82d1919f5873e
jra2995 <jettanderson@utexas.edu>
2016-04-25 17:01:44 -0500

    Game Logic Pretty Much Done... I think.
    Simple Game Logic Done, Need to Put It all Together With Input/Output to
    Console. But I think generating the pegs is done.

--- src/mastermind/Game.java
+++ src/mastermind/Game.java
@@ -75,8 +75,128 @@ public class Game {
 		return numGuesses;
 	}
 	
-	public String evaluateGuess(String guess){
+	public boolean validGuess(String guess){
+		if(guess.length() != secretCode.length()){
+			return false;
+		}
 		
-		return "";
+		for(int i = 0; i < guess.length(); i++){
+			boolean isColor = false;
+			for(int j = 0; j < validColors.size(); j++){
+				if(guess.charAt(i) == validColors.get(j)){
+					isColor = true;
+					break;
+				}
+			}
+			if(!isColor){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public String evaluateGuess(String guess){
+		String copyCode = secretCode;
+		String result1 = checkBlackPegs(guess);
+		int numBlackPegs = calculateNumBlackPegs(result1);
+		String result2 = checkWhitePegs(result1);
+		int numWhitePegs = calculateNumWhitePegs(result2, numBlackPegs);
+		String answer = "";
+		
+		if(testing){
+			answer = answer + testOutput(copyCode) + "\nResult: ";
+		}
+		else{
+			answer = "Result: ";
+		}
+		
+		if(numBlackPegs == 0 && numWhitePegs == 0){
+			answer = answer + "No pegs";
+		}
+		else if(numBlackPegs == 0){
+			answer = answer + numWhitePegs + " white pegs";
+		}
+		else if(numWhitePegs == 0){
+			answer = answer + numBlackPegs + " black pegs";
+		}
+		else{
+			answer = answer + numBlackPegs + " black pegs and " + numWhitePegs + " white pegs";
+		}
+		
+		secretCode = copyCode;
+		return answer;
+	}
+	
+	public String testOutput(String originalCode){
+		String output = "The secret code is : " + originalCode + ". The edited code is " + secretCode;
+		return output;
+	}
+	
+	private String checkBlackPegs(String guess){
+		for(int i = 0; i < secretCode.length(); i++){
+			if(secretCode.charAt(i) == guess.charAt(i) && i != guess.length() - 1){
+				guess = guess.substring(0, i) + "-" + guess.substring(i + 1);
+				secretCode = secretCode.substring(0, i) + "-" + secretCode.substring(i + 1);
+			}
+			else if(secretCode.charAt(i) == guess.charAt(i)){
+				guess = guess.substring(0, i) + "-";
+				secretCode = secretCode.substring(0, i) + "-";
+			}
+		}
+		
+		return guess;
+	}
+	
+	private int calculateNumBlackPegs(String guess){
+		int num = 0;
+		for(int i = 0; i < guess.length(); i++){
+			if(guess.charAt(i) == '-'){
+				num++;
+			}
+		}
+		
+		return num;
+	}
+	
+	private String checkWhitePegs(String guess){
+		for(int i = 0; i < guess.length(); i++){
+			if(guess.charAt(i) == '-'){
+				continue;
+			}
+			else{
+				for(int j = 0; j < secretCode.length(); j++){
+					if(secretCode.charAt(j) == '-'){
+						continue;
+					}
+					else{
+						if(guess.charAt(i) == secretCode.charAt(j)){
+							char[] tmp = secretCode.toCharArray();
+							tmp[j] = '-';
+							secretCode = new String(tmp);
+							char[] tmp2 = guess.toCharArray();
+							tmp2[i] = '-';
+							guess = new String(tmp2);
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		return guess;
+	}
+	
+	private int calculateNumWhitePegs(String guess, int black){
+		int numColoredPegs = 0;
+		for(int i = 0; i < guess.length(); i++){
+			if(guess.charAt(i) == '-'){
+				numColoredPegs++;
+			}
+		}
+		
+		int numWhite = numColoredPegs - black;
+		
+		return numWhite;
 	}
 }
